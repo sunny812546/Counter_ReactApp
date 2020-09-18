c10cc16e945d36e74804b738b2eb7b819ec0376c
sunny812546 <sun812546@gmail.com>
2020-09-18 11:42:43 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./Button";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const Increment = () => {
+    return count > 10 ? 11 : setCount(count + 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>This is Counter App Using Hooks</h1>
       </header>
+      <h2>
+        Current Counter Value: <span>{count}</span>
+      </h2>
+      <Button
+        title="Reset Counter"
+        onClick={() => {
+          setCount(0);
+        }}
+      />
+      <Button title="Increment Counter" onClick={Increment} />
+      <Button
+        title="Decrement Counter"
+        onClick={() => (count < 1 ? 0 : setCount(count - 1))}
+      />
     </div>
   );
-}
+};
 
 export default App;
